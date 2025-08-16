cf14b0adcba67f663df8697cd3c468c382daefad
Arash Dolati Mehr <arashdm2020@gmail.com>
2025-08-16 16:35:59 +0330

    Fix Google Fonts ETIMEDOUT issue by removing Google Fonts and keeping Vazir font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "زینوا - پلتفرم هوشمند توسعه دارو",
@@ -35,13 +24,9 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased font-vazir`}
-      >
+      <body className="antialiased font-vazir">
         <div className="min-h-screen flex flex-col">
           <Header />
           <main className="flex-grow">
